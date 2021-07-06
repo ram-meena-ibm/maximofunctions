6327e1866ab2645d2bc86cfbb91dcc6977d39a2a
ram.meena <ram.meena@in.ibm.com>
2021-07-06 15:20:27 +0530

    new function added

---
+++ customTempConverter/functions.py
@@ -0,0 +1,52 @@
+import inspect
+import logging
+import datetime as dt
+import math
+from sqlalchemy.sql.sqltypes import TIMESTAMP,VARCHAR
+import numpy as np
+import pandas as pd
+
+from iotfunctions.base import BaseTransformer
+from iotfunctions import ui
+
+logger = logging.getLogger(__name__)
+
+# Specify the URL to your package here.
+# This URL must be accessible via pip install.
+# Example assumes the repository is private.
+# Replace XXXXXX with your personal access token.
+# After @ you must specify a branch.
+
+PACKAGE_URL = 'git+https://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@github.com/ram-meena-ibm/maximofunctions@beta'
+
+class updateTempFunction(BaseTransformer):
+
+    def __init__(self, input_items, factor, output_items):
+
+        self.input_items = input_items
+        self.output_items = output_items
+        self.factor = float(factor)
+        super().__init__()
+    def execute(self, df):
+        df = df.copy()
+        for i,input_item in enumerate(self.input_items):
+            df[self.output_items[i]] = df[input_item] * self.factor
+        return df
+
+    @classmethod
+    def build_ui(cls):
+        #define arguments that behave as function inputs
+        inputs = []
+        inputs.append(ui.UIMultiItem(
+                name = 'input_items',
+                datatype=float,
+                description = "Data items adjust",
+                output_item = 'output_items',
+                is_output_datatype_derived = True)
+                      )
+        inputs.append(ui.UISingle(
+                name = 'factor',
+                datatype=float)
+                      )
+        outputs = []
+        return (inputs,outputs)
